2eb658e825d13d3389b1deac457f02edd5a0a789
HHHMGK <65905035+HHHMGK@users.noreply.github.com>
2022-09-22 21:52:01 +0700

    Create Sprite and AssetsPool

--- src/main/java/components/Sprite.java
+++ src/main/java/components/Sprite.java
@@ -1,6 +1,36 @@
 package components;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class Sprite extends Component {
-    // TODO: complete this class
-    
+    public String path;
+    public BufferedImage image;
+    int width, height;
+    int row, col;
+
+    public Sprite(String path) {
+        this.path = path;
+        try {
+//            File file = new File(path);
+            image = ImageIO.read(new File(path));
+            width = image.getWidth();
+            height = image.getHeight();
+        } catch (Exception e) {
+            e.printStackTrace();
+//            System.out.println("Can't load image file at: " + path);
+//            System.exit(-1);
+        }
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        int x = row;
+        int y = col;
+        g2.drawImage(image, x, y, width, height, null);
+    }
+
+
 }
--- src/main/java/util/AssetsPool.java
+++ src/main/java/util/AssetsPool.java
@@ -1,5 +1,39 @@
 package util;
 
+import components.Sprite;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AssetsPool {
-    // TODO: complete this class
+    static Map<String, Sprite> sprites = new HashMap<>();
+
+    public static boolean hasSprite(String path) {
+        File file = new File(path);
+        return AssetsPool.sprites.containsKey(file.getAbsolutePath());
+    }
+
+    public static Sprite getSprite(String path) {
+        File file = new File(path);
+        if (AssetsPool.hasSprite(path)) {
+            return AssetsPool.sprites.get(file.getAbsolutePath());
+        } else {
+            Sprite sprite = new Sprite(path);
+            AssetsPool.addSprite(path,sprite);
+            return AssetsPool.sprites.get(file.getAbsolutePath());
+        }
+    }
+
+    public static void addSprite(String path, Sprite sprite) {
+        File file = new File(path);
+        if (AssetsPool.hasSprite(path)) {
+            System.out.println("Already added sprite at: " + file.getAbsolutePath());
+//            System.exit(-1);
+        } else {
+            AssetsPool.sprites.put(file.getAbsolutePath(), sprite);
+        }
+    }
+
+
 }
