ffdfaeeba99cf7ba20d4fcc8e169029d993b82dc
HHHMGK <65905035+HHHMGK@users.noreply.github.com>
2022-09-23 12:46:15 +0700

    Changed how Sprite and AssetsPool work

--- src/main/java/util/AssetsPool.java
+++ src/main/java/util/AssetsPool.java
@@ -1,39 +1,45 @@
 package util;
 
 import components.Sprite;
+import components.SpriteSheet;
 
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AssetsPool {
-    private static Map<String, Sprite> sprites = new HashMap<>();
+    //    private static Map<String, Sprite> sprites = new HashMap<>();
+//
+//    public static Sprite getSprite(String path) {
+//        path = (new File(path)).getAbsolutePath();
+//        if (!AssetsPool.sprites.containsKey(path)) {
+//            Sprite sprite = new Sprite(path);
+//            AssetsPool.addSprite(path,sprite);
+//        }
+//        return AssetsPool.sprites.get(path);
+//    }
+//
+//    public static void addSprite(String path, Sprite sprite) {
+//        path = (new File(path)).getAbsolutePath();
+//        if (AssetsPool.sprites.containsKey(path)) {
+//            System.out.println("Already added sprite at: " + path);
+//        } else {
+//            AssetsPool.sprites.put(path, sprite);
+//        }
+//    }
+    private static Map<String, SpriteSheet> pool = new HashMap<>();
 
-    public static boolean hasSprite(String path) {
-        File file = new File(path);
-        return AssetsPool.sprites.containsKey(file.getAbsolutePath());
-    }
-
-    public static Sprite getSprite(String path) {
-        File file = new File(path);
-        if (AssetsPool.hasSprite(path)) {
-            return AssetsPool.sprites.get(file.getAbsolutePath());
+    public static void addSpriteSheet(String path, SpriteSheet sheet) {
+        path = (new File(path)).getAbsolutePath();
+        if (AssetsPool.pool.containsKey(path)) {
+            System.out.println("Already added asset at: " + path);
         } else {
-            Sprite sprite = new Sprite(path);
-            AssetsPool.addSprite(path,sprite);
-            return AssetsPool.sprites.get(file.getAbsolutePath());
+            AssetsPool.pool.put(path, sheet);
         }
     }
 
-    public static void addSprite(String path, Sprite sprite) {
-        File file = new File(path);
-        if (AssetsPool.hasSprite(path)) {
-            System.out.println("Already added sprite at: " + file.getAbsolutePath());
-//            System.exit(-1);
-        } else {
-            AssetsPool.sprites.put(file.getAbsolutePath(), sprite);
-        }
+    public static SpriteSheet getSpriteSheet(String path) {
+        path = (new File(path)).getAbsolutePath();
+        return AssetsPool.pool.get(path);
     }
-
-
 }
