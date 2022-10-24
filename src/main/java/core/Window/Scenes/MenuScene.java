7ba92f71046f0688510c23c4efe9cc612a6fa59c
DucHiepisme <113037834+DucHiepisme@users.noreply.github.com>
2022-10-24 17:02:00 +0700

    button update and fix

--- src/main/java/core/Window/Scenes/MenuScene.java
+++ src/main/java/core/Window/Scenes/MenuScene.java
@@ -6,31 +6,40 @@ import core.GameObject.components.SpriteSheet;
 import core.MouseController;
 import util.AssetsPool;
 import util.Prefabs;
-import util.Vector2D;
+import util.Box2D;
 
 import java.awt.Graphics2D;
 
 public class MenuScene extends Scene {
-    
+
     public MenuScene() {
         super();
     }
 
     @Override
     public void init() {
-        GameObject play = Prefabs.generateButton(Prefabs.readImage("src/main/resources/LargeButton/Large_Buttons/Start_Button.png"),
-                Prefabs.readImage("src/main/resources/LargeButton/Colored_Large_Buttons/Start_col_Button.png"));
-        play.setTransform(new Transform(new Vector2D(300, 300), 0));
-        addGameObject(play);
+        //init menu Start
+        GameObject menuStart = Prefabs.generateButton(Prefabs.readImage("src/main/resources/menuImg/hover_buttons/start.png"),
+                Prefabs.readImage("src/main/resources/menuImg/idle_buttons/start.png"));
+        menuStart.setTransform(new Transform(new Box2D(300, 300, 0, 0), 0));
+        addGameObject(menuStart);
+
+        //init menu Exit
+
+        GameObject menuExit = Prefabs.generateButton(Prefabs.readImage("src/main/resources/menuImg/hover_buttons/exit.png"),
+                Prefabs.readImage("src/main/resources/menuImg/idle_buttons/start.png"));
+        menuExit.setTransform(new Transform(new Box2D(300, 450, 0, 0), 0));
+        addGameObject(menuExit);
+
     }
-    
+
     @Override
     public void update(double dt) {
         for (GameObject g : gameObjects) {
             g.update(dt);
         }
     }
-    
+
     @Override
     public void draw(Graphics2D g2D) {
         renderer.render(g2D);
@@ -38,13 +47,13 @@ public class MenuScene extends Scene {
 
     @Override
     public void load_resources() {
-        SpriteSheet startButtonBlack = new SpriteSheet("src/main/resources/LargeButton/Large_Buttons/Start_Button.png", 0, 0, 1);
+        SpriteSheet startButtonBlack = new SpriteSheet("src/main/resources/menuImg/hover_buttons/start.png", 0, 0, 1);
         AssetsPool.addSpriteSheet(startButtonBlack.getPath(), startButtonBlack);
-        SpriteSheet exitButtonBlack = new SpriteSheet("src/main/resources/LargeButton/Large_Buttons/Exit_Button.png", 0, 0, 1);
+        SpriteSheet exitButtonBlack = new SpriteSheet("src/main/resources/menuImg/hover_buttons/exit.png", 0, 0, 1);
         AssetsPool.addSpriteSheet(exitButtonBlack.getPath(), startButtonBlack);
-        SpriteSheet startButtonRed = new SpriteSheet("src/main/resources/LargeButton/Colored_Large_Buttons/Start_col_Button.png", 0, 0, 1);
+        SpriteSheet startButtonRed = new SpriteSheet("src/main/resources/menuImg/idle_buttons/start.png", 0, 0, 1);
         AssetsPool.addSpriteSheet(startButtonRed.getPath(), startButtonRed);
-        SpriteSheet exitButtonRed = new SpriteSheet("src/main/resources/LargeButton/Colored_Large_Buttons/Exit_col_Button.png", 0, 0, 1);
+        SpriteSheet exitButtonRed = new SpriteSheet("src/main/resources/menuImg/idle_buttons/exit.png", 0, 0, 1);
         AssetsPool.addSpriteSheet(exitButtonRed.getPath(), exitButtonRed);
     }
 }
